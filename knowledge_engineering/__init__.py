1a9e2db105103555229dc09d718fe161b47bae2d
SriGanesh-123 <sriganeshmarimuthu93@gmail.com>
2026-08-24 00:36:47 +0530

    Expose investigation agent from package

--- knowledge_engineering/__init__.py
+++ knowledge_engineering/__init__.py
@@ -1,5 +1,6 @@
 """Knowledge Engineering canonicalization and validation package."""
 
+from .investigation_agent import InvestigationAgent
 from .rag_service import RAGService
 
-__all__ = ["RAGService"]
+__all__ = ["InvestigationAgent", "RAGService"]
